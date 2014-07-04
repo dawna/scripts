c940f251671518b17b26b1cdd4e03c25d29741ce
Ben Reul <breul99@gmail.com>
2014-07-03 19:02:01 -0500

    Create screenBrightness.sh

---
+++ screenBrightness.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Screen brightness script for ux301LA
+if [ "$#" == "0" ]
+	then
+		echo "needs an argument"
+		exit
+fi
+if [ "$(whoami)" != "root" ]
+	then
+		echo "gotta be root"
+		exit
+fi
+
+echo $1 | tee /sys/class/backlight/acpi_video0/brightness 
+
+exit

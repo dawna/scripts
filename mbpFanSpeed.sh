76ff2de10e46c2e46c92fb0bdccb7dd812c73134
Ben Reul <breul99@gmail.com>
2014-07-09 02:28:56 -0500

    Update mbpFanSpeed.sh

--- mbpFanSpeed.sh
+++ mbpFanSpeed.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 # manually change the fanspeed on a macbook pro 5,5
-if [ "$(id -u)" != "0" ]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
+if [ "$#" == "0" ]
+   then
+   echo "Needs speed arg where 0<speed<9000"
+   exit
 fi
-if [ "$#" -ne 1 ]; then
-    echo "Needs speed arg where 0<speed<9000"
-    exit 1
+if [ "$(whoami)" != "root" ]
+   then
+   echo "gotta be root"
+   exit
 fi
 su -c 'echo 1 > /sys/devices/platform/applesmc.768/fan1_manual'
 su -c "echo $1 > /sys/devices/platform/applesmc.768/fan1_output"
